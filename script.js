3ab5537e01ac9df3026a5676f676b8b7056d17c5
Manny <immanuelmonroe@gmail.com>
2023-03-07 22:22:35 -0500

    Added contructor and new book function

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 }
+
+const book = new Book('Bible', 'God', '1200', 'yes')
+console.log(book.title)
 
 function addBookToLibrary() {
   // do stuff here
