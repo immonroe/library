74a10766c771cb5fae377c9211c2793eeff27271
immonroe <immanuelmonroe@gmail.com>
2023-03-28 20:35:40 -0400

    Created two book objects/added prototype functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let myLibrary = [];
+let myLibrary = []
 
-function Book(title, author, pages, read) {
+function Book (title, author, pages, read) {
   // the constructor...
   this.title = title
   this.author = author
@@ -8,9 +8,29 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-const book = new Book('Bible', 'God', '1200', 'yes')
-console.log(book.title)
+Book.prototype.sayTitle = function () {
+  console.log(this.title)
+}
+
+Book.prototype.sayAuthor = function () {
+  console.log(this.author)
+}
+
+Book.prototype.sayPages = function () {
+  console.log(this.pages)
+}
+
+Book.prototype.sayRead = function () {
+  console.log(this.read)
+}
+
+const bookOne = new Book('Bible', 'God', '1200', 'yes')
+bookOne.sayTitle()
+
+const bookTwo = new Book('Lord of the Rings', 'J.R.R. Tolkien', '1178', 'no')
+bookTwo.sayAuthor()
 
 function addBookToLibrary() {
   // do stuff here
+  // create function to loop over items and add them to myLibrary array
 }
