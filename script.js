b744d68ee39f8428d24898a1762c4d59f45d5b3a
immonroe <immanuelmonroe@gmail.com>
2023-03-29 14:09:05 -0400

    Added prompts to addBook function

--- script.js
+++ script.js
@@ -12,13 +12,18 @@ function Book (title, author, pages, isRead) {
 }
 
 // Function to add a book to the library
-function addBookToLibrary(title, author, pages, isRead) {
-  let book = new Book(title, author, pages, isRead);
-  library.books.push(book);
+function addBookToLibrary (title, author, pages, isRead) {
+  // let title = prompt("Enter the book's title:");
+  // let author = prompt("Enter the book's author:");
+  // let pages = parseInt(prompt("Enter the number of pages:"));
+  // let isRead = confirm("Have you read this book?"); // bug where prompts keep looping and there is no option to verify if book is read through prompt
+
+  let book = new Book(title, author, pages, isRead)
+  library.books.push(book)
 }
 
 // Function to remove a book from the library
-function removeBookFromLibrary(title) {
+function removeBookFromLibrary (title) {
   for (let i = 0; i < library.books.length; i++) {
     if (library.books[i].title === title) {
       library.books.splice(i, 1)
@@ -26,3 +31,14 @@ function removeBookFromLibrary(title) {
     }
   }
 }
+
+addBookToLibrary('Bible', 'God', 1200, true)
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false)
+addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 281, false)
+addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', 277, true)
+
+// console.log(library.books) // Output: [Book, Book, Book, Book]
+
+removeBookFromLibrary('To Kill a Mockingbird')
+
+console.log(library.books) // Output: [Book, Book, Book]
