783f8d810327fc72b00e4b7ac1d3daad1a70bbec
immonroe <immanuelmonroe@gmail.com>
2023-04-01 10:27:25 -0400

    Added button to add books to library

--- script.js
+++ script.js
@@ -13,14 +13,28 @@ function Book (title, author, pages, isRead) {
 
 // Function to add a book to the library
 function addBookToLibrary (title, author, pages, isRead) {
-  // let title = prompt("Enter the book's title:");
-  // let author = prompt("Enter the book's author:");
-  // let pages = parseInt(prompt("Enter the number of pages:"));
-  // let isRead = confirm("Have you read this book?"); // bug where prompts keep looping and there is no option to verify if book is read through prompt
-
   let book = new Book(title, author, pages, isRead)
   library.books.push(book)
+  // Create a button that calls the function with a popout asking for information?
+  displayBooks()
 }
+
+function displayBooks() {
+  let bookList = document.getElementById("book-list");
+  bookList.innerHTML = "";
+
+  for (let i = 0; i < library.books.length; i++) {
+    let book = library.books[i];
+
+    let bookInfo = document.createElement("p");
+    bookInfo.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}, Read: ${book.isRead}`;
+
+    bookList.appendChild(bookInfo);
+  }
+}
+
+let addBookButton = document.getElementById("add-book");
+addBookButton.addEventListener("click", addBookToLibrary);
 
 // Function to remove a book from the library
 function removeBookFromLibrary (title) {
@@ -36,9 +50,3 @@ addBookToLibrary('Bible', 'God', 1200, true)
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false)
 addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 281, false)
 addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', 277, true)
-
-// console.log(library.books) // Output: [Book, Book, Book, Book]
-
-removeBookFromLibrary('To Kill a Mockingbird')
-
-console.log(library.books) // Output: [Book, Book, Book]
