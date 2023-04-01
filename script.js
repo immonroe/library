78e876d14edcad870ab68866d39fd6778210f23d
immonroe <immanuelmonroe@gmail.com>
2023-04-01 11:05:45 -0400

    Fixed bug where books were not displaying upon load

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-// function displayBooks() {
-//   let bookList = document.getElementById("book-list");
-//   bookList.innerHTML = "";
-
-//   for (let i = 0; i < library.books.length; i++) {
-//     let book = library.books[i];
-
-//     let bookInfo = document.createElement("p");
-//     bookInfo.textContent = `Title: ${book.title}, Author: ${book.author}, Pages: ${book.pages}, Read: ${book.isRead}`;
-
-//     bookList.appendChild(bookInfo);
-//   }
-// }
-
-// let addBookButton = document.getElementById("add-book");
-// addBookButton.addEventListener("click", addBookToLibrary);
-
-// // Function to remove a book from the library
-// // function removeBookFromLibrary (title) {
-// //   for (let i = 0; i < library.books.length; i++) {
-// //     if (library.books[i].title === title) {
-// //       library.books.splice(i, 1)
-// //       break
-// //     }
-// //   }
-// // }
-
-
-
 // Create a library object with an empty books array
 const library = {
-  books: []
+  books: [
+    {
+      title: "Mere Christianity",
+      author: "C.S. Lewis",
+      pages: 227,
+      isRead: false
+    },
+    {
+      title: "The Ruthless Elimination of Hurry",
+      author: "John Mark Comer",
+      pages: 304,
+      isRead: true
+    },
+    {
+      title: "Atomic Habits",
+      author: "James Clear",
+      pages: 320,
+      isRead: true
+    }
+  ]
 };
 
 // Book constructor function
@@ -61,10 +51,20 @@ function addBookToLibrary() {
   // Pressing okay = true | Pressing cancel = false
 
   let book = new Book(title, author, pages, isRead);
-  library.books.push(book);
+  library.books.push(book); // New books created will be added to books array in library object
 
   displayBooks();
 }
+
+// Function to remove a book from the library
+// function removeBookFromLibrary (title) {
+//   for (let i = 0; i < library.books.length; i++) {
+//     if (library.books[i].title === title) {
+//       library.books.splice(i, 1)
+//       break
+//     }
+//   }
+// }
 
 function displayBooks() {
   let bookList = document.getElementById("book-list");
@@ -80,10 +80,11 @@ function displayBooks() {
   }
 }
 
+// Alternate way to add books to library upon load of page (sample books)
+// library.books.push(new Book("The Catcher in the Rye", "J.D. Salinger", 234, false));
+// library.books.push(new Book("Pride and Prejudice", "Jane Austen", 432, true));
+
+displayBooks();
+
 let addBookButton = document.getElementById("add-book");
 addBookButton.addEventListener("click", addBookToLibrary);
-
-// // addBookToLibrary('Bible', 'God', 1200, true)
-// // addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false)
-// // addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', 281, false)
-// // addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', 277, true)
