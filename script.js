9606e8054c49c48424ba8ca4d5dcd69027f8ccba
immonroe <immanuelmonroe@gmail.com>
2023-07-04 09:13:10 -0400

    Refactored code and replaces original constructors with classes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// selectors
+// Selectors
 const addBookButton = document.getElementById("add-book"); // opens modal menu
 const bookModal = document.getElementById("book-modal");
 const closeBtn = document.querySelector(".close");
@@ -21,36 +21,65 @@ window.addEventListener("click", function(event) {
   }
 });
 
-// Create a library object with an empty books array
-const library = {
-  books: [
-    {
-      title: "Mere Christianity",
-      author: "C.S. Lewis",
-      pages: 227,
-      isRead: false
-    },
-    {
-      title: "The Ruthless Elimination of Hurry",
-      author: "John Mark Comer",
-      pages: 304,
-      isRead: true
-    },
-    {
-      title: "Atomic Habits",
-      author: "James Clear",
-      pages: 320,
-      isRead: true
-    }
-  ]
-};
+class Library {
+  constructor() {
+    this.books = [
+      {
+        title: "Mere Christianity",
+        author: "C.S. Lewis",
+        pages: 227,
+        isRead: false
+      },
+      {
+        title: "The Ruthless Elimination of Hurry",
+        author: "John Mark Comer",
+        pages: 304,
+        isRead: true
+      },
+      {
+        title: "Atomic Habits",
+        author: "James Clear",
+        pages: 320,
+        isRead: true
+      },
+      {
+        title: "New Book 1",
+        author: "Author 1",
+        pages: 200,
+        isRead: false
+      },
+      {
+        title: "New Book 2",
+        author: "Author 2",
+        pages: 150,
+        isRead: true
+      }
+    ];
+  }
 
-// Book constructor function
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
+  addBook(book) {
+    this.books.push(book);
+  }
+
+  removeBook(book) {
+    const index = this.books.indexOf(book);
+    if (index !== -1) {
+      this.books.splice(index, 1);
+    }
+  }
+}
+
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+
+  toggleReadStatus() {
+    this.isRead = !this.isRead;
+  }
 }
 
 function displayBooks() {
@@ -83,21 +112,20 @@ function displayBooks() {
     bookRead.textContent = book.isRead ? "Read" : "Unread";
     bookRead.style.backgroundColor = book.isRead ? "green" : "red";
     bookRead.addEventListener("click", function() {
-      book.isRead = !book.isRead; // toggle the isRead status
+      book.toggleReadStatus();
       this.textContent = book.isRead ? "Read" : "Unread";
       this.style.backgroundColor = book.isRead ? "green" : "red";
     });
     bookInfo.appendChild(bookRead);
 
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('remove-button');
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-      const index = library.books.indexOf(book);
-      library.books.splice(index, 1);
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove-button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", () => {
+      library.removeBook(book);
       displayBooks();
     });
-    
+
     bookInfo.appendChild(removeButton);
 
     bookList.appendChild(bookInfo);
@@ -120,9 +148,9 @@ function addBookToLibrary() {
     alert("Please fill in all required fields.");
     return;
   }
-  
+
   // Add new book to library
-  library.books.push(book);
+  library.addBook(book);
 
   // Clear form and close modal
   addBookForm.reset();
@@ -131,6 +159,9 @@ function addBookToLibrary() {
   // Refresh book list
   displayBooks();
 }
+
+// Create a library object
+const library = new Library();
 
 // Add event listener to add book button
 addBookBtn.addEventListener("click", addBookToLibrary);
