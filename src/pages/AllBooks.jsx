00206a4f635828bdbceba87974e74f8e259e51ec
immonroe <immanuelmonroe@gmail.com>
2023-08-01 18:45:01 -0400

    Mapped dummy books to main page

--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -1,7 +1,36 @@
+const DUMMY_DATA = [
+
+    {
+      id: 'm1',
+      title: 'This is the first book',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      Author: 'Testing',
+      description:
+        'A really good book',
+    },
+    {
+      id: 'm2',
+      title: 'This is the second book',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'YAYA YEET',
+      description:
+        'This book changed my life!',
+    },
+];
+
 function AllBooks() {
     return (
-        <div>All Books</div>
-    )
-}
+      <section>
+        <h1>All Meetups</h1>
+        <ul>
+          {DUMMY_DATA.map((book) => {
+            return <li key={book.id}>{book.title}</li>;
+          })}
+        </ul>
+      </section>
+    );
+  }
 
 export default AllBooks;
