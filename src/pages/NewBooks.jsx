fd7226056d6c9e3ee3876a1caa67b0e37c8bc074
immonroe <immanuelmonroe@gmail.com>
2023-08-05 19:44:59 -0400

    Added useNavigate hook/submitting new book returns home

--- src/pages/NewBooks.jsx
+++ src/pages/NewBooks.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom'
+
 import NewBookForm from "../components/layout/books/NewBookForm";
 
 function NewBooks() {
+    const navigate = useNavigate();
+
     function addMeetupHandler(meetupData) {
         fetch(
             'https://library-react-app-7dd08-default-rtdb.firebaseio.com/books.json',
@@ -11,14 +15,17 @@ function NewBooks() {
                     'Content-Type': 'application/json'
                 }
             }
-            );
+        )
+        .then(() => {
+            navigate('/');
+        });
     }
 
     return (
-        <section>
-            <div>New Books</div>
+        <div>
+            <h2>Add New Book</h2>
             <NewBookForm onAddMeetup={addMeetupHandler} />
-        </section>
+        </div>
     );
 }
 
