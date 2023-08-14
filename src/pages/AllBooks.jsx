07c6c0de4790f7710cc54d3f319d6aa4e95fa3fe
immonroe <immanuelmonroe@gmail.com>
2023-08-13 23:21:09 -0400

    Removed dummy data and configured firebase to pull info

--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -1,31 +1,45 @@
+import { useState, useEffect } from 'react';
+
 import BookList from "../components/layout/books/BookList";
 
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is the first book',
-      image:
-        'https://images.pexels.com/photos/1290141/pexels-photo-1290141.jpeg',
-      author: 'Manny',
-      description:
-        'Great book, changed my life and helped me discover my passion for underwater basket weaving!',
-    },
-    {
-      id: 'm2',
-      title: 'This is the second book',
-      image:
-        'https://images.pexels.com/photos/1290141/pexels-photo-1290141.jpeg',
-      author: 'Generic author name',
-      description:
-        'This book was honestly terrible, not sure who decided to write it but hey I finished it so it must have been somewhat enjoyable',
-    },
-];
-
 function AllBooks() {
+    const[isLoading, setIsLoading] = useState(true);
+    const[loadedMeetups, setLoadedMeetups] = useState([])
+
+    useEffect(() => {
+      setIsLoading(true);
+      fetch('https://library-react-app-7dd08-default-rtdb.firebaseio.com/books.json'
+      )
+        .then(response => {
+          return response.json();
+        }).
+        then((data) => {
+          const meetups = [];
+
+          for (const key in data) {
+            const meetup = {
+              id: key,
+              ...data[key]
+            }
+
+            meetups.push(meetup)
+          }
+
+          setIsLoading(false)
+          setLoadedMeetups(meetups)
+        });
+    }, []); 
+
+    if (isLoading) {
+      <section>
+        <p>Loading...</p>
+      </section>
+    }
+
     return (
       <section>
         <h1>All Books</h1>
-        <BookList books={DUMMY_DATA} />
+        <BookList books={loadedMeetups} />
       </section>
     );
   }
