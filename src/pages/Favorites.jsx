f519794c81839275e6ac48d75358b0cd0b59a2f5
immonroe <immanuelmonroe@gmail.com>
2023-08-14 18:26:17 -0400

    Added favorites functionality/nav badge

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,23 @@
-function Favorites() {
-    return (
-        <div>Favorites</div>
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import BookList from "../components/books/BookList";
 
+function Favorites() {
+    const favoritesCtx = useContext(FavoritesContext)
+
+    let content;
+
+    if (favoritesCtx.totalFavorites === 0) {
+        content = <p>You currently have no favorites.</p>
+    } else {
+       content = <BookList books={favoritesCtx.favorites} />
+    }
+
+    return (
+        <section>
+            <h1>My Favorites</h1>
+            {content}
+        </section>
     )
 }
 
