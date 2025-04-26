41bcbbef9c47669a113f5ee02865fe2de78d9613
immonroe <immanuelmonroe@gmail.com>
2025-04-26 09:29:27 -0400

    Reverted .env database.js changes

---
+++ config/database.js
@@ -0,0 +1,16 @@
+// config/database.js
+module.exports = {
+    'url' : 'mongodb+srv://library:library@cluster0.s9pjnd9.mongodb.net/demo?retryWrites=true&w=majority&appName=Cluster0', 
+    'dbName': 'demo'
+};
+
+// require('dotenv').config();
+
+// const dbUser = process.env.DB_USER;
+// const dbPass = process.env.DB_PASS;
+// const dbName = process.env.DB_NAME;
+
+// module.exports = {
+//     url: `mongodb+srv://${dbUser}:${dbPass}@cluster0.y8wfsvj.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`,
+//     dbName: dbName
+// };
